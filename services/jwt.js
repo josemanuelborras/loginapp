52009f078978dbecdf337b496d383b38f6eb7fee
jomiborras <josemanuel.borras@gmail.com>
2021-11-12 12:26:35 -0300

    jwt service

---
+++ services/jwt.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const jwt = require('jwt-simple');
+const moment = require('moment');
+const configs = require('../config.json');
+const secret = configs.pass;
+
+exports.createToken = (user)=>{
+    let payload = {
+        sub: user._id,
+        name: user.name,
+        last_name: user.last_name,
+        email: user.email,
+        iat: moment().unix(),
+        exp: moment().add(30, 'days').unix()
+    };
+
+    return jwt.encode(payload, secret);
+};
