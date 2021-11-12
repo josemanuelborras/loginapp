d4d28f21fe98242a28e66d40dce71cb911918552
jomiborras <josemanuel.borras@gmail.com>
2021-11-12 12:09:33 -0300

    app.js created

---
+++ app.js
@@ -0,0 +1,32 @@
+'use strict'
+
+// EXPRESS CONNECTION
+const express = require('express');
+
+// EXPRESS CALL
+const app = express();
+
+// ROUTES CONFIG
+const user_routes = require('./routes/userRoutes');
+
+// MIDDLEWARES
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+
+    next();
+});
+
+// ROUTES
+const route = '/';
+
+app.use(route, user_routes);
+
+// EXPORTS
+module.exports = app;
